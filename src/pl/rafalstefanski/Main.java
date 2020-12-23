6510a95081ee6192381478937149780b9138d045
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-23 15:04:38 +0100

    341. The Music SQLite Database (@MSI)

--- src/pl/rafalstefanski/Main.java
+++ src/pl/rafalstefanski/Main.java
@@ -1,8 +1,16 @@
 package pl.rafalstefanski;
 
+import pl.rafalstefanski.model.Datasource;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Datasource datasource = new Datasource();
+        if (!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
     }
 }
