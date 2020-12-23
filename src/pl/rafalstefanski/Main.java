6a298d79b2105cbab13250b84bf23c4ea726c1bc
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-23 16:28:18 +0100

    342. Write Java Query for Artists (@MSI)

--- src/pl/rafalstefanski/Main.java
+++ src/pl/rafalstefanski/Main.java
@@ -1,6 +1,9 @@
 package pl.rafalstefanski;
 
+import pl.rafalstefanski.model.Artist;
 import pl.rafalstefanski.model.Datasource;
+
+import java.util.List;
 
 public class Main {
 
@@ -9,6 +12,16 @@ public class Main {
         if (!datasource.open()) {
             System.out.println("Can't open datasource");
             return;
+        }
+
+        List<Artist> artists = datasource.queryArtists();
+        if (artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
 
         datasource.close();
--- src/pl/rafalstefanski/model/Datasource.java
+++ src/pl/rafalstefanski/model/Datasource.java
@@ -1,15 +1,15 @@
 package pl.rafalstefanski.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
 
     public static final String CONNECTION_STRING = "jdbc:sqlite:C:\\Users\\RS\\IdeaProjects\\Music\\" + DB_NAME;
 
-    public static final String  TABLE_ALBUMS = "albums";
+    public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
     public static final String COLUMN_ALBUM_NAME = "name";
     public static final String COLUMN_ALBUM_ARTISTS = "artists";
@@ -42,6 +42,27 @@ public class Datasource {
             }
         } catch (SQLException e) {
             System.out.println("Couldn't close connection: " + e.getMessage());
+        }
+    }
+
+    public List<Artist> queryArtists() {
+
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS)) {
+
+            List<Artist> artists = new ArrayList<>();
+            while (results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTISTS_ID));
+                artist.setName(results.getString(COLUMN_ARTISTS_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+
+        } catch (SQLException e) {
+            System.out.println("Query failed: " + e.getMessage());
+            return null;
         }
     }
 }
