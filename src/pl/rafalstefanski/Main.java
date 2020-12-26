c6ac2156b23bd35da6a26b06ca8c96fb7eb6c4b8
Rafał Stefański <rafal-stefanski@wp.pl>
2020-12-26 15:46:44 +0100

    348. Write the Method to Query View (@M)

--- src/pl/rafalstefanski/Main.java
+++ src/pl/rafalstefanski/Main.java
@@ -51,6 +51,18 @@ public class Main {
 
         datasource.createViewForSongArtists();
 
+        songArtists = datasource.querySongInfoView("Go Your Own Way");
+        if (songArtists.isEmpty()) {
+            System.out.println("Couldn't find the artist for the song");
+            return;
+        }
+
+        for (SongArtist artist : songArtists) {
+            System.out.println("FROM VIEW  - Artist name = " + artist.getArtistName() +
+                    " Album name = " + artist.getAlbumName() +
+                    " Track Number = " + artist.getTrack());
+        }
+
         datasource.close();
     }
 }
